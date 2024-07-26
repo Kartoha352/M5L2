ff53c9930244ea24d1e33928a4970647890becbe
kartoha352 <anton_motry@mail.ru>
2024-07-26 16:28:28 +0300

    v1.0

---
+++ bot.py
@@ -0,0 +1,48 @@
+import telebot
+from config import *
+from logic import *
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Привет! Я бот, который может показывать города на карте. Напиши /help для списка команд.")
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message.chat.id, "Доступные команды:\n/start - начать работу с ботом и получить приветственное сообщение.\n/help - получить список доступных команд.\n/show_city <city_name> - отобразить указанный город на карте.\n/remember_city <city_name> - сохранить город в список избранных.\n/show_my_cities - показать все сохраненные города.")
+
+
+@bot.message_handler(commands=['show_city'])
+def handle_show_city(message):
+    city_name = " ".join(message.text.split()[1:])
+    # Реализуй отрисовку города по запросу
+    user_id = message.chat.id
+    manager.create_graph(f'{user_id}.png', [city_name])  # Создание карты для города
+    with open(f'{user_id}.png', 'rb') as map:  # Открытие и отправка карты пользователю
+        bot.send_photo(user_id, map)
+
+
+
+
+@bot.message_handler(commands=['remember_city'])
+def handle_remember_city(message):
+    user_id = message.chat.id
+    city_name = " ".join(message.text.split()[1:])
+    if manager.add_city(user_id, city_name):
+        bot.send_message(message.chat.id, f'Город {city_name} успешно сохранен!')
+    else:
+        bot.send_message(message.chat.id, 'Такого города я не знаю. Убедись, что он написан на английском!')
+
+@bot.message_handler(commands=['show_my_cities'])
+def handle_show_visited_cities(message):
+    cities = manager.select_cities(message.chat.id)
+    # Реализуй отрисовку всех городов
+    user_id = message.chat.id
+    manager.create_graph(f'{user_id}.png', cities)  # Создание карты для города
+    with open(f'{user_id}.png', 'rb') as map:  # Открытие и отправка карты пользователю
+        bot.send_photo(user_id, map)
+
+if __name__=="__main__":
+    manager = DB_Map(DATABASE)
+    bot.polling()
